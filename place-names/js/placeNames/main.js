5b83b936b57789ea2037d1d544b5afd9d8cc0354
Pazia Ramirez <pazia008@gmail.com>
2021-03-01 10:37:36 -0600

    finished place names exercise

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,17 @@
 // Put your code here
+console.log("All Place Names")
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+
+
+for (let name of names) {
+    console.log(name)
+}
+
+
+console.log("'The' Place Names")
+const theNames = names.filter((n) => n.includes("The"));
+
+for (let name of theNames) {
+    console.log(name)
+}
