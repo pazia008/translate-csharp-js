7974a67f4a9b2e29c10981087c02501e4cf76aff
Pazia Ramirez <pazia008@gmail.com>
2021-03-05 13:45:33 -0600

    listed out my wishes

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,28 @@
 // Put your code here
+console.log(`Let's roll some dice, baby!`)
+console.log(`---------------------------`)
+
+// for (int i = 0; i < 10; i++)
+// {
+//     Die die1 = Roll();
+//     Die die2 = Roll();
+
+//     string message = $"{die1} + {die2} == {die1.Value + die2.Value}";
+//     if (die1.Value == die2.Value)
+//     {
+//         message += " DOUBLES!";
+//     }
+
+//     Console.WriteLine(message);
+// }
+
+for (let i = 0; i < 10; i++) {
+    const dieOne = Roll()
+    const dieTwo = Roll()
+
+    if (dieOne === dieTwo) {
+        console.log(`${dieOne} + ${dieTwo} == ${dieOne.Value + dieTwo.Value} DOUBLES!`)
+    } else {
+        console.log(`${dieOne} + ${dieTwo} == ${dieOne.Value + dieTwo.Value}`)
+    }
+}
